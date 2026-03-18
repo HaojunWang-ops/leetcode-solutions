b7ac0dedf277458e5ed81faa5a4071edca404307
wanghaojun <3148214638@qq.com>
2026-03-18 20:06:03 +0800

    soulution for 19

--- 19.cpp
+++ 19.cpp
@@ -3,23 +3,41 @@
  *
  * [19] 删除链表的倒数第 N 个结点
  */
+#include <iostream>
+#include <stdio.h>
+#include <string.h>
+
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
 
 // @lc code=start
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
-class Solution {
+class Solution
+{
 public:
-    ListNode* removeNthFromEnd(ListNode* head, int n) {
-        
+    ListNode *removeNthFromEnd(ListNode *head, int n)
+    {
+        ListNode* dummy = new ListNode(0, head);
+        ListNode* left_pointer = dummy;
+        ListNode* right_pointer = dummy;
+        int i = 0;
+        while (i <= n){
+            i++;
+            right_pointer = right_pointer->next;
+        }
+        while (right_pointer != nullptr){
+            right_pointer = right_pointer->next;
+            left_pointer = left_pointer->next;
+        }
+
+        left_pointer->next = left_pointer->next->next;
+
+        return dummy->next;
     }
 };
 // @lc code=end
-
